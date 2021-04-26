596c56569ad4d477b53dac7f8cd0b09458e4e6bb
Brosner522 <rosnerbb@gmail.com>
2021-04-26 15:05:04 -0400

    commit tree cd684877a5093d3d9e77a2f8ec55c54de8323b82

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import CatComponent from `./CatComponent.js`
+import {GraceHopperQuoteComponent} from `./GraceHopperQuoteComponent.js`
+import MouseComponent from `./MouseComponent.js`
+
+
 
 export default class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
         <img src="/cat.gif" />
+        <CatComponent />
+        <GraceHopperQuotesComponent />
+        <MouseComponent />
       </div>
     );
   }
